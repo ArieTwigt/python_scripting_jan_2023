45382ff1730ca0c09305961eadf8cb8714641724
Arie Twigt <ariekanarie@twigtd4.nl>
2023-01-12 10:19:10 +0100

    added examles for chapter 4

---
+++ assignments_4.py
@@ -0,0 +1,32 @@
+#%% Assignment 1
+person = {}
+person['name'] = 'Jim'
+person['age'] = 50
+person['hobbies'] = ['cycling', 'surfing', 'reading']
+
+#%% Assignment 2
+person_2 = {}
+person_2['name'] = 'Mary'
+person_2['age'] = 48
+person_2['hobbies'] = ['programming', 'teaching']
+
+person_3 = {}
+person_3['name'] = 'Marco'
+person_3['age'] = 13
+person_3['hobbies'] = ['gaming', 'football']
+
+person_4 = {}
+person_4['name'] = 'Ann'
+person_4['age'] = 17
+person_4['hobbies'] = ['blogging', 'tennis']
+
+
+
+# some other persons
+
+# family dictionary
+family = {}
+family['name'] = "Jones"
+family['members'] = [person, person_2, person_3, person_4]
+
+# %%
