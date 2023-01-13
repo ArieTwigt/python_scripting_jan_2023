5e60530dd994629e0bf5c0357eba3ac8d652f458
Arie Twigt <ariekanarie@twigtd4.nl>
2023-01-13 13:54:18 +0100

    added example for password input

--- exp_9_input_debug.py
+++ exp_9_input_debug.py
@@ -1,8 +1,12 @@
 from handy_functions.calculation_functions import calc_pythagoras
+from getpass import getpass
+
+password = getpass("Insert password:\n")
 
 a_value = float(input("Provide a value for a (10):\n") or 10)
 b_value = float(input("Provide a value for b (10):\n") or 10)
 
+print(password)
 
 result = calc_pythagoras(a_value, b_value)
 print(result)
